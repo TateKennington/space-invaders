e92ea80b938d13c8d14d3ce0f8dd055d1b072d30
TateKennington <TateKennington@users.noreply.github.com>
2018-06-19 20:05:04 +1200

    Created project and added basic engine functionality

---
+++ SpaceInvaders/Engine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    class Engine
+    {
+        Graphics g;
+        int t;
+
+        public Engine()
+        {
+            t = 0;
+        }
+
+        public void SetGraphics(Graphics _g)
+        {
+            g = _g;
+        }
+
+        public void Tick()
+        {
+
+        }
+
+        public void Render()
+        {
+            g.Clear(Color.Black);
+        }
+    }
+}
